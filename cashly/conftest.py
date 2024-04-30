bc2d27db6e0cdca4083e821a4feb484e12454554
Ibrahim E.Gad <eibrahim95@gmail.com>
2024-04-30 19:19:55 +0300

    Initialize a plain cookiecutter django project

---
+++ cashly/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from cashly.users.models import User
+from cashly.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture()
+def user(db) -> User:
+    return UserFactory()
